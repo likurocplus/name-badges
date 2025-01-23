21ffe5ecaef785af59543668bba514e34eca0ffc
khoiquoc <likuroquoc@gmail.com>
2025-01-23 07:34:47 +0700

    update touppercase name

--- src/components/Badge.jsx
+++ src/components/Badge.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 
 const Badge = ({ data }) => {
+  let lastname = data["Last Name"].toUpperCase();
+  let firstname = data["First Name"].toUpperCase();
   return (
     <div className="w-full">
       <p className="font-bold text-3xl text-cyan-900 block text-center">
-        {data["Last Name"] + " " + data["First Name"]}
+        {lastname + " " + firstname}
       </p>
       <br />
       <p className="text-2xl block text-center">{data["Job Title"]}</p>
